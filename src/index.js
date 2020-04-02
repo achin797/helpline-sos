a090b1f0bc5cb4dd2a164416efab0f6a420a0cc9
achin <achintya.chandwani@gmail.com>
2020-04-02 15:30:11 -0700

    Login page - basic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './pages/Login';
 import * as serviceWorker from './serviceWorker';
+import './styles/global/main.less';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
